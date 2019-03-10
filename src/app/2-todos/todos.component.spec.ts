ef2455b522f64d96617c10ea4155d9e364f48f1b
Kleiton Marques <kleiton.88design@gmail.com>
2019-03-10 13:21:00 -0300

    Getting the Dependencies

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { HttpModule } from '@angular/http'
+import { Observable } from 'rxjs'
 
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service'
@@ -23,10 +24,14 @@ describe('TodosComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load todos from the server', () => {
+    let service = TestBed.get(TodoService);
+    spyOn(service, 'getTodos').and.returnValue(Observable.from([ [1, 2, 3] ]));
+
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
   });
 });
