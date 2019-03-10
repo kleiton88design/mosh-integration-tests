687ff46d70682e4d9ffebffe4e3fde24387d0e07
Kleiton Marques <kleiton.88design@gmail.com>
2019-03-10 13:36:41 -0300

    providing stubs

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -2,27 +2,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
 
 import { UserDetailsComponent } from './user-details.component';
 
-xdescribe('UserDetailsComponent', () => {
-  let component: UserDetailsComponent;
-  let fixture: ComponentFixture<UserDetailsComponent>;
+class RouterStub {
+	navigate(params) {
+	}
+}
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
-    })
-    .compileComponents();
-  }));
+class ActivatedRoutesStub {
+	params: Observable<any> = Observable.empty();
+}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+describe('UserDetailsComponent', () => {
+	let component: UserDetailsComponent;
+	let fixture: ComponentFixture<UserDetailsComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ UserDetailsComponent ],
+			providers: [
+				{ provide: Router, useClass: RouterStub },
+				{ provide: ActivatedRoute, useClass: ActivatedRoutesStub },
+			]
+		})
+		.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UserDetailsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
