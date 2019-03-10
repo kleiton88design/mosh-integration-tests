2adadb13c7897ef84677dea92dae7d7e1b395fb6
Kleiton Marques <kleiton.88design@gmail.com>
2019-03-10 12:41:36 -0300

    testing event bindings

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -30,9 +30,15 @@ describe('VoterComponent', () => {
 	it('should highlight the upvoted button if i have upvoted', () => {
 		component.myVote = 1;
 		fixture.detectChanges();
-
 		let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
 
 		expect(de.classes['highlighted']).toBeTruthy();
+	});
+
+	it('should increase total votes when I click the upvote button', () => {
+		let button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+		button.triggerEventHandler('click', null);
+
+		expect(component.totalVotes).toBe(1);
 	});
 });
